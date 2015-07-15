dacb8707936587c39820f296cecd43bf2d38569e
Tamiros <Tamiros@ubuntu>
2015-07-15 08:25:50 -0700

    change puffed map

--- BehaviorsProj/Map/Map.cpp
+++ BehaviorsProj/Map/Map.cpp
@@ -57,34 +57,17 @@ Map::Map() {
 	// Creating large map for puff the original map
 	int largeMap[height][width];
 
-	/*// Running over the matrix map and puffy it
-	 for (y = 1; y < height-1; y++){
-	 for (x = 1; x < width-1; x++) {
-	 if (map[y][x] == 1) {
-	 largeMap[y-1][x-1] = 1;
-	 largeMap[y-1][x] = 1;
-	 largeMap[y-1][x+1] = 1;
-	 largeMap[y][x-1] = 1;
-	 largeMap[y][x+1] = 1;
-	 largeMap[y+1][x-1] = 1;
-	 largeMap[y+1][x] = 1;
-	 largeMap[y+1][x+1] = 1;
-	 }
-	 }
-	 }*/
-
 	// Running over the matrix map and puffy it
-	//TODO: check if we need to puff full size
 	for (int y = 0; y < height; y++) {
 		for (int x = 0; x < width; x++) {
 			// Check if there is black cell
 			if (map[y][x] == BLOCK) {
 
 				// Run over the cell to puff
-				for (int pY = y - puffSize / 2; pY < y + puffSize / 2; pY++) {
+				for (int pY = y - puffSize; pY < y + puffSize; pY++) {
 					// Check if we didnt exceed the matrix
 					if (pY >= 0 && pY < height) {
-						for (int pX = x - puffSize / 2; pX < x + puffSize / 2;
+						for (int pX = x - puffSize; pX < x + puffSize;
 								pX++) {
 							// Check if we didnt exceed the matrix
 							if (pX >= 0 && pX < width) {
@@ -178,21 +161,6 @@ Map::Map() {
 	this->_grid = Grid(grid_height, grid_width, gridResolution, height, width, startPosition, goalPoint);
 	this->_grid.initGridByMatrix(grid);
 
-	//printing the grid to file to check it
-	FILE* fgrid = fopen("grid.txt", "w");
-	if (fgrid == NULL) {
-		printf("Error opening file!\n");
-		exit(1);
-	}
-	for (int y = 0; y < grid_height; y++) {
-		for (int x = 0; x < grid_width; x++) {
-			fprintf(fgrid, "%d", grid[y][x]);
-			//cout << map[y][x];
-		}
-		fprintf(fgrid, "\n");
-		//cout << endl;
-	}
-	fclose(fgrid);
 }
 
 Grid Map::getGrid() {
